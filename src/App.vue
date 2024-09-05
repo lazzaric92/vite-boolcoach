f0a52aff66fe7abfb75f6f24c67fbb33151cd4ed
lazzaric92 <lazzaric_92@hotmail.it>
2024-09-05 19:04:15 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+@use '../node_modules/bootstrap/scss/bootstrap.scss';
+@use './assets/styles/general.scss';
+@use './assets/styles/partials/variables' as *;
+@use './assets/styles/partials/mixins' as *;
+</style>
---
+++ src/components/partials/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default{
+    data(){
+        return {
+            message: 'Header'
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>{{ message }}</h1>
+        <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+        <nav>
+            <!-- <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink> -->
+        </nav>
+    </header>
+</template>
+
+<style scoped lang="scss">
+
+</style>
