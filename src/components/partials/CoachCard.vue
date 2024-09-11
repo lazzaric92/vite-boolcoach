ff705ffdd64ccdd1f20ca77de9ee8313a3b3b728
lazzaric92 <lazzaric_92@hotmail.it>
2024-09-11 11:46:25 +0200

    added getCoachesList function, passed prop to CoachCard.vue

---
+++ src/components/partials/CoachCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default{
+    data(){
+        return {
+            
+        }
+    },
+    props: {
+        singleCoach: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="border border-light rounded">
+        <p class="text-white">{{ singleCoach.nickname }}</p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/components/partials/CoachesList.vue
+++ src/components/partials/CoachesList.vue
@@ -1,18 +1,50 @@
 <script>
+import axios from 'axios';
+import CoachCard from './CoachCard.vue';
+
 export default{
     data(){
         return {
-            message: 'Coaches'
+            message: 'Coaches',
+            coachesList: [],
         }
     },
+    components: {
+        CoachCard
+    },
     methods: {
-        // getCoachesIndex
+        getCoachesList() {
+            axios.get('http://127.0.0.1:8000/api/coaches',{
+                params: {
+
+                }
+            })
+            .then((response) =>{
+                console.log(response.data.results);
+                this.coachesList = response.data.results;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+    },
+    created(){
+        this.getCoachesList();
     }
 }
 </script>
 
 <template>
 <h1> {{ message }} </h1>
+<div class="container-fluid">
+    <div class="row">
+        <article class="col-3 mb-3" v-for="coach in coachesList" key="coach.id">
+            <CoachCard :singleCoach="coach"/>
+        </article>
+        
+    </div>
+</div>
+
 </template>
 
 <style scoped lang="scss">
