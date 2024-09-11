7bec84b459747af89b7edb11e3bb3742072fb8fc
CodeYouness <ylijassi@gmail.com>
2024-09-11 17:19:30 +0200

    minority changes

--- src/components/pages/guests/AppGame.vue
+++ src/components/pages/guests/AppGame.vue
@@ -15,41 +15,20 @@ export default{
 
 <template>
     <div class="container py-4">
-        <form class="row justify-content-around mb-4 p-3">
-            <!-- ! GAME_ID -->
-            <div class="col-3">
-                <select class="form-select" id="game_id" name="game_id" >
-                    <option selected disabled>-- Select a game --</option>
-                    <option value="1">League of Legends</option>
-                    <option value="2">Tom Clancy's Rainbow Six Siege</option>
-                    <option value="3">FIFA 25</option>
-                    <option value="4">Overwatch</option>
-                    <option value="5">Rocket League</option>
-                </select>
-            </div>
-            
-            <!-- ! VOTE_AVG -->
-            <div class="col-3">
-                <select class="form-select" id="vote_avg" name="vote_avg">
-                    <option selected disabled>-- Vote --</option>
-                    <option value="1">1 star or higher</option>
-                    <option value="2">2 stars or higher</option>
-                    <option value="3">3 stars or higher</option>
-                    <option value="4">4 stars or higher</option>
-                    <option value="5">5 stars</option>
-                </select>
-            </div>
-            
-            <!-- ! NICKNAME -->
-            <div class="col-4">
-                <input class="form-control me-2" name="nickname" type="search" placeholder="Coach name" aria-label="Search">
-            </div>
-            
-            <div class="col-1">
-                <button class="btn text-white" type="submit">Search</button>
-            </div>
-        </form>
-        <CoachesList />
+        <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
 </template>
 
