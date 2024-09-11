d5be64da72778cbfd774af9df6e98197114fe2b8
lazzaric92 <lazzaric_92@hotmail.it>
2024-09-11 17:52:16 +0200

    fixed api call

--- src/components/pages/guests/AppSearch.vue
+++ src/components/pages/guests/AppSearch.vue
@@ -1,5 +1,7 @@
 <script>
+import CoachCard from '@/components/partials/CoachCard.vue';
 import CoachesIndex from '@/components/partials/CoachesIndex.vue';
+import axios from 'axios';
 
 export default{
     data(){
@@ -8,22 +10,26 @@ export default{
             gameId: '',
             voteAvg: '',
             nicknameString: '',
+            string: '',
+            searchResults: []
         }
     },
     components: {
-        CoachesIndex
+        CoachesIndex,
+        CoachCard
     },
     methods: {
-        getSearchedCoaches(game, vote, nickname){
-            axios.get('',{
-                    params:{
-                    game_id: game,
-                    vote_avg: vote,
-                    nickname: nickname
-                }
+        getSearchedCoaches(game, vote, nick){
+            axios.get('http://127.0.0.1:8000/api/coaches/search',{
+                params:{
+                game_id: game,
+                vote_avg: vote,
+                nickname: nick
+            }
             })
             .then((response) => {
                 console.log(response.data.results);
+                this.searchResults = response.data.results;
             })
             .catch((error) =>{
                 console.log(error);
@@ -37,9 +43,9 @@ export default{
     <div class="container py-4">
         <div class="row justify-content-around mb-4 p-3">
             <!-- ! GAME_ID -->
-            <div class="col-3">
-                <select class="form-select" id="game_id" name="game_id" v-model="gameId">
-                    <option selected disabled>-- Select a game --</option>
+            <div class="col-3 d-flex justify-content-center">
+                <select id="game_id" v-model="this.gameId">
+                    <option value="" selected disabled>-- Select a game --</option>
                     <option value="1">League of Legends</option>
                     <option value="2">Tom Clancy's Rainbow Six Siege</option>
                     <option value="3">FIFA 25</option>
@@ -49,9 +55,9 @@ export default{
             </div>
             
             <!-- ! VOTE_AVG -->
-            <div class="col-3">
-                <select class="form-select" id="vote_avg" name="vote_avg" v-model="voteAvg">
-                    <option selected disabled>-- Vote --</option>
+            <div class="col-3 d-flex justify-content-center">
+                <select id="vote_avg" v-model="this.voteAvg">
+                    <option value="" selected disabled>-- Vote --</option>
                     <option value="1">1 star or higher</option>
                     <option value="2">2 stars or higher</option>
                     <option value="3">3 stars or higher</option>
@@ -62,15 +68,21 @@ export default{
             
             <!-- ! NICKNAME -->
             <div class="col-4">
-                <input class="form-control me-2" name="nickname" type="search" placeholder="Coach name" aria-label="Search" v-model="nicknameString">
+                <input class="form-control me-2" name="nickname" type="search" placeholder="Coach name" aria-label="Search" v-model="this.nicknameString">
             </div>
             
             <div class="col-1">
-                <button class="btn text-white" @click="[getSearchedCoaches(gameId, voteAvg, nicknameString), searchOn = true]">Search</button>
+                <button class="btn text-white" @click="[console.log(this.game_id, this.vote_avg, this.nickname), getSearchedCoaches(this.gameId, this.voteAvg, this.nicknameString), searchOn = true]">Search</button>
             </div>
-            <p v-if="searchOn === true" class="col-12 text-white text-decoration-underline p-1 mb-0" @click="[searchOn = false, gameId = '', voteAvg = '', nicknameString = '']">Remove filters</p>
+            <p v-if="searchOn === true" id="clear-search" class="col-12 text-white text-decoration-underline p-1 mb-0" @click="[searchOn = false, gameId = '', voteAvg = '', nicknameString = '']">Remove filters</p>
+            <p class="text-white">params:  <span>{{ gameId }}</span> <span>{{ voteAvg }}</span> {{ nicknameString }}</p>
         </div>
         <CoachesIndex v-if="searchOn === false"/>
+        <div v-else class="row">
+        <article class="col-3 mb-3" v-for="coach in searchResults" key="coach.id">
+            <CoachCard :singleCoach="coach"/>
+        </article>
+    </div>
     </div>
 </template>
 
@@ -94,5 +106,9 @@ export default{
             border: 1px solid transparent;
             scale: 1.05;
         }
+    }
+
+    p#clear-search {
+        cursor: pointer;
     }
 </style>
