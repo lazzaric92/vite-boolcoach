54049b429bd477955cd5f07f1d90a4a55028c326
AdelMysterio <storliniadelmo@gmail.com>
2024-09-11 14:30:20 +0200

    completato componente AppJumbo

--- src/components/pages/guests/AppHome.vue
+++ src/components/pages/guests/AppHome.vue
@@ -1,19 +1,27 @@
 <script>
+import AppJumbo from '@/components/partials/AppJumbo.vue';
+import AppMainHome from '@/components/partials/AppMainHome.vue';
+
+
 export default {
     data() {
         return {
-            message: 'Home'
+            message: ''
         }
+    },
+    components: {
+        AppJumbo,
+        AppMainHome
     }
 }
 </script>
 
 <template>
-    <h1 class="text-white"> {{ message }} </h1>
+    <main>
+        <AppJumbo/>
+        <AppMainHome />
+    </main>
 </template>
 
 <style lang="scss">
-@use '../../../assets/styles/general.scss' as *;
-
-
 </style>
