33f7ad87769723a86e1c2f61a07001f1ecec4d1a
CodeYouness <ylijassi@gmail.com>
2024-09-12 11:16:08 +0200

    first add of login form

--- src/components/pages/guests/AppLogin.vue
+++ src/components/pages/guests/AppLogin.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from 'axios';
 
 export default {
     data() {
@@ -13,8 +14,32 @@ export default {
 </script>
 
 <template>
-
+<body class="d-flex">
+    <div class="col-6 mx-auto text-white">
+        <form>
+            <!-- Email input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="email" id="form2Example1" class="form-control" />
+                <label class="form-label" for="form2Example1">Email address</label>
+            </div>
+    
+            <!-- Password input -->
+            <div data-mdb-input-init class="form-outline mb-4">
+                <input type="password" id="form2Example2" class="form-control" />
+                <label class="form-label" for="form2Example2">Password</label>
+            </div>
+    
+    
+        <!-- Submit button -->
+            <button  type="button" class="btn btn-primary btn-block mb-4">Login</button>
+        </form>
+    </div>
+    
+</body>
 </template>
 
 <style scoped lang="scss">
+body {
+    height: 100%;
+}
 </style>
