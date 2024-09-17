173d249ae6c0c832f3ebc0d3b8d70e8520635383
CodeYouness <ylijassi@gmail.com>
2024-09-17 15:46:50 +0200

    added composer braintree

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f9bb293c2f408b4c805ad7ece89c1c583c61941d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f9bb293c2f408b4c805ad7ece89c1c583c61941d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'braintree/braintree_php' => array(
+            'pretty_version' => '6.19.0',
+            'version' => '6.19.0.0',
+            'reference' => 'f3178632ca098d1f96a429d665aabc4e95346c03',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../braintree/braintree_php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
